8bb58b1d1f045f4026b2932ae75d2ee09e913b27
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-26 13:05:44 -0300

    create project

---
+++ src/test/java/com/marcos/breakfast/BreakfastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.breakfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakfastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
