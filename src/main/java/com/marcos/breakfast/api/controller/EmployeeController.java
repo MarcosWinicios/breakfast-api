ec57a3c33c8c454e0e45a231dda684c7e4c7e81c
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-26 20:31:32 -0300

    add data persistence

--- src/main/java/com/marcos/breakfast/api/controller/EmployeeController.java
+++ src/main/java/com/marcos/breakfast/api/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.marcos.breakfast.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcos.breakfast.domain.model.Employee;
 
 
 @RestController
@@ -10,8 +15,22 @@ public class EmployeeController {
 	
 	
 	@GetMapping("/employees")
-	public String listar() {
+	public List<Employee> listar() {
+		var employee1 = new Employee(1L, "Marcos Winicios Pereira	", "11111111111");
+		var employee2 = new Employee(2L, "João Alves", "22222222222");
+
+		var employee3 = new Employee(3L, "Bill Gates", "33333333333");
+
+		var employee4 = new Employee(4L, "Steve Jobs", "44444444444");
+
 		
-		return "teste";
+		return Arrays.asList(employee1, employee2, employee3, employee4);
 	}
+	
+	@GetMapping("/employee")
+	public Employee findById(Long id) {
+		
+		return new Employee(1L, "Marcos Winicios", "11111111111");
+	}
+	
 }
---
+++ src/main/resources/db/migration/V001__create-employee-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employee(
+	id bigint not null auto_increment,
+	name varchar(60) not null,
+	cpf varchar(11) not null,
+	primary key(id)
+);
