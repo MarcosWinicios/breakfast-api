71f9f4637dc6ef3c53b51e16e090287ab200c688
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-26 21:05:16 -0300

    add ORM annotations

--- src/main/java/com/marcos/breakfast/api/controller/EmployeeController.java
+++ src/main/java/com/marcos/breakfast/api/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.marcos.breakfast.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,25 +14,14 @@ import com.marcos.breakfast.domain.model.Employee;
 @RestController
 public class EmployeeController {
 	
-	
+	@PersistenceContext
+	private EntityManager manager;
 	
 	@GetMapping("/employees")
 	public List<Employee> listar() {
-		var employee1 = new Employee(1L, "Marcos Winicios Pereira	", "11111111111");
-		var employee2 = new Employee(2L, "João Alves", "22222222222");
-
-		var employee3 = new Employee(3L, "Bill Gates", "33333333333");
-
-		var employee4 = new Employee(4L, "Steve Jobs", "44444444444");
-
 		
-		return Arrays.asList(employee1, employee2, employee3, employee4);
+		return manager.createQuery("from employee", Employee.class).getResultList();
 	}
-	
-	@GetMapping("/employee")
-	public Employee findById(Long id) {
-		
-		return new Employee(1L, "Marcos Winicios", "11111111111");
-	}
+
 	
 }
--- src/main/java/com/marcos/breakfast/domain/model/Employee.java
+++ src/main/java/com/marcos/breakfast/domain/model/Employee.java
@@ -1,8 +1,24 @@
 package com.marcos.breakfast.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="employee")
 public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name="name")
 	private String name;
+	
+	@Column(name="cpf")
 	private String cpf;
 	
 	public Employee() {}
@@ -36,6 +52,28 @@ public class Employee {
 	}
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
-	}	
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", cpf=" + cpf + "]";
+	}
 }
  
