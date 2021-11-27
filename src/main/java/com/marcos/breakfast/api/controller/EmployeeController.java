bb611ee440dafa3e69e497e1b549d860541eb741
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-26 22:01:46 -0300

    implementation of the registration of employees

--- src/main/java/com/marcos/breakfast/api/controller/EmployeeController.java
+++ src/main/java/com/marcos/breakfast/api/controller/EmployeeController.java
@@ -1,15 +1,16 @@
 package com.marcos.breakfast.api.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcos.breakfast.domain.model.Employee;
@@ -40,5 +41,11 @@ public class EmployeeController {
 //			return ResponseEntity.ok(result.get());
 //		}
 //		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Employee save(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 }
