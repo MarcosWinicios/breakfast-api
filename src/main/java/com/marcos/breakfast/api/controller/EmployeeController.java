b7431cee0a690d760f196983cf399cfa397c2926
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-26 22:24:54 -0300

    employee removal implementation

--- src/main/java/com/marcos/breakfast/api/controller/EmployeeController.java
+++ src/main/java/com/marcos/breakfast/api/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,16 @@ public class EmployeeController {
 		employee = employeeRepository.save(employee);
 		return ResponseEntity.ok(employee);
 	}
+	
+	@DeleteMapping("/{employeeId}")
+	public ResponseEntity<Void> remove(@PathVariable Long employeeId){
+		if(!employeeRepository.existsById(employeeId)) {
+			return ResponseEntity.notFound().build();
+		}
+		employeeRepository.deleteById(employeeId);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 
 }
