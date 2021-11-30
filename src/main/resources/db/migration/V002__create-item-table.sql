d9dbfa27bca2936ba8cdb19d9301e27171627b74
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-30 09:33:12 -0300

    change for postgres database

--- src/main/resources/db/migration/V002__create-item-table.sql
+++ src/main/resources/db/migration/V002__create-item-table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS tb_item(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(60) NOT NULL,
-    employee_id BIGINT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (employee_id) REFERENCES tb_employee(id)
+  id BIGSERIAL NOT NULL,
+  name VARCHAR(60) NOT NULL,
+  employee_id BIGINT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY (employee_id) REFERENCES tb_employee(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
 );
-
--- src/main/resources/db/migration/V003__add-cascade-on-delete-and-update.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE tb_item 
-    DROP FOREIGN KEY tb_item_ibfk_1;
-ALTER TABLE tb_item 
-    ADD CONSTRAINT tb_item_ibfk_1 
-    FOREIGN KEY (employee_id) 
-    REFERENCES tb_employee(id) 
-    ON DELETE CASCADE 
-    ON UPDATE CASCADE;
-
