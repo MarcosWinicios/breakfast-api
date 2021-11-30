21c255752647cbcb58fc8b1e09440644d4e8c1a2
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-30 09:39:09 -0300

    create inserts file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO tb_employee (name, cpf) VALUES('Marcos Winicios', '11111111111');
+INSERT INTO tb_employee (name, cpf) VALUES('Joao Alves', '22222222222');
+INSERT INTO tb_employee (name, cpf) VALUES('Bill Gates', '33333333333');
+INSERT INTO tb_employee (name, cpf) VALUES('Steve Jobs', '44444444444');
+INSERT INTO tb_employee (name, cpf) VALUES('Linus Turvalds', '55555555555');
+INSERT INTO tb_employee (name, cpf) VALUES('Linus Turvalds', '66666666666');
+
+INSERT INTO tb_item (name, employee_id) VALUES ('Pão de queijo', 1);
+INSERT INTO tb_item (name, employee_id) VALUES ('Café', 1);
+INSERT INTO tb_item (name, employee_id) VALUES ('Bolo de cenoura', 2);
+INSERT INTO tb_item (name, employee_id) VALUES ('Leite', 4);
+INSERT INTO tb_item (name, employee_id) VALUES ('Pão Francês', 2);
+INSERT INTO tb_item (name, employee_id) VALUES ('Achocolatado', 2);
